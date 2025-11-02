351b00e13a6e53b2b9f4efbea565f5959c5f9b95
Max <scorpiomax90@gmail.com>
2025-11-02 21:30:27 +0200

    Added components (header,footer,home)

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,14 +2,20 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Note } from './models/note.model';
 import { CommonModule } from '@angular/common';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
+  handleSubscribe() {
+    console.log('work click');
+  }
+
   protected readonly notes = signal<Note[]>([
     {
       id: 1,
