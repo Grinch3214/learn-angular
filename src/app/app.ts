50b9a7c1efa508edbb3e9b609eb097d6ca119072
Max <scorpiomax90@gmail.com>
2025-11-02 20:56:22 +0200

    Added test array and use template for view content (use pipe, ngFor)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Note } from './models/note.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('learn-angular');
+  protected readonly notes = signal<Note[]>([
+    {
+      id: 1,
+      title: 'First note',
+      content: 'Hi, this is first note',
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      title: 'Second note',
+      content: 'Hi, this is second note',
+      createdAt: new Date(),
+    },
+  ]);
 }
