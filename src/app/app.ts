2a476fb861f4733a4da79f3386cceed66ce676e2
Max <scorpiomax90@gmail.com>
2025-11-28 17:02:51 +0200

    Refactor components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Note } from './models/note.model';
 import { CommonModule } from '@angular/common';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { FormsModule } from '@angular/forms';
-import { NoteService } from './services/note';
 
 @Component({
   selector: 'app-root',
@@ -14,37 +12,9 @@ import { NoteService } from './services/note';
   styleUrl: './app.scss',
 })
 export class App {
-  constructor(private noteService: NoteService) {}
-
   handleSubscribe() {
     console.log('work click');
   }
 
   title = 'Test title (file app.ts)';
-
-  protected notes() {
-    return this.noteService.getNotes()();
-  }
-
-  showConsole() {
-    console.log('Click h1');
-  }
-
-  protected newNote = signal<Partial<Note>>({
-    title: '',
-    content: '',
-  });
-
-  protected addNote() {
-    const noteData = this.newNote();
-
-    if (!noteData.title || !noteData.content) return;
-
-    this.noteService.addNote(noteData.title, noteData.content);
-
-    this.newNote.set({
-      title: '',
-      content: '',
-    });
-  }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NoteService } from '../../services/note';
+import { Note } from '../../models/note.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  constructor(private noteService: NoteService) {}
 
+  protected notes() {
+    return this.noteService.getNotes()();
+  }
+
+  showConsole() {
+    console.log('Click h1');
+  }
+
+  protected newNote = signal<Partial<Note>>({
+    title: '',
+    content: '',
+  });
+
+  protected addNote() {
+    const noteData = this.newNote();
+
+    if (!noteData.title || !noteData.content) return;
+
+    this.noteService.addNote(noteData.title, noteData.content);
+
+    this.newNote.set({
+      title: '',
+      content: '',
+    });
+  }
 }
