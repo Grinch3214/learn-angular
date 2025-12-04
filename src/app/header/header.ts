5fabef18bc0f08f7129f46714f0786f0f6517a66
Max <scorpiomax90@gmail.com>
2025-12-04 16:16:46 +0200

    Refactor scss and create header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,23 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+interface NavItem {
+  name: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
   @Input() title!: string;
+
+  list: NavItem[] = [
+    { name: 'Home', url: '/' },
+    { name: 'About', url: '/about' },
+  ];
 }
