427e4486e8b8756e0cd8c00060c563548f98efe2
Max <scorpiomax90@gmail.com>
2026-01-18 22:02:49 +0200

    Create movies service

--- src/app/services/movies.ts
+++ src/app/services/movies.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,4 +9,10 @@ import { environment } from '../../environments/environment';
 export class Movies {
   private apiUrl: string = environment.omdbApiUrl;
   private apiKey: string = environment.omdbApiKey;
+
+  constructor(private https: HttpClient) {}
+
+  searchMovies(title: string): Observable<any> {
+    return this.https.get<any>(`${this.apiUrl}?=${title}&apikey=${this.apiKey}`);
+  }
 }
