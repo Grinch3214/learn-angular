770da5d66511c48daf01aa90c6b8b9324af3a470
Max <scorpiomax90@gmail.com>
2026-01-18 21:58:02 +0200

    Added environment files

--- src/app/services/movies.ts
+++ src/app/services/movies.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Movies {
-  
+  private apiUrl: string = environment.omdbApiUrl;
+  private apiKey: string = environment.omdbApiKey;
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  omdbApiKey: '25cd110e',
+  omdbApiUrl: 'http://www.omdbapi.com/',
+};
